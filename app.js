f1584dbc429d14b1eec4c5131fd7a200646d6da2
Alessandro Di Giusto <108011478+Alessandro-DiGiusto@users.noreply.github.com>
2023-09-07 23:17:09 +0200

    new version with post and get features

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 8000; // this is our port number
+const fs = require('fs');
+const bookingRoutes = require('./routes/bookings');
+
+
+//NEW
+const bodyParser = require('body-parser');
+//NEW
+
+const app = express(); // this is our app or instance of express
+
+
+// API Middelwares
+app.use(express.json()); //this is to accept data in json format
+app.use(express.urlencoded()); // this is basically to decode the data send through html form
+app.use(express.static('public'));
+app.use(cors());
+
+//NEW
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use('/prenota', bookingRoutes);
+
+//NEW
+
+// API ROUTES
+app.get('/', (request, responseC) => {
+  const message = req.query.message || '';
+  res.sendFile(__dirname + '/public/index.html');
+  responseC.sendFile(path.join(__dirname, "style.css"))
+});
+
+// This is Basically to listen on port 
+app.listen(port, () => {
+  console.log('Server started at http://localhost:${port}')
+});
+
+
